950bb927231dbe2a73c54580dfa7bf478b5bed0c
Adam <141070325+adamsayshi@users.noreply.github.com>
2025-09-29 13:50:52 -0400

    first

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DistanceConverter {
     public static void main(String[] args) {
         // TODO: Implement distance converter
@@ -7,5 +9,36 @@ public class DistanceConverter {
         // - Use conversion factor: 1 mile = 1.60935 kilometers
         // - Display conversion results
         // - Handle invalid menu choices
+        Scanner sc = new Scanner(System.in);
+        boolean cont = true;
+        System.out.println("Welcome to Distance converter");
+        int user = 0;
+        double initial = 0;
+        double fin = 0;
+        while (cont){
+            System.out.println("Convert to 1. Kilometers 2. Miles or 3. Leave:  ");
+            user = sc.nextInt();
+            switch (user){
+                case 1:
+                    System.out.println("Enter Miles:  ");
+                    initial = sc.nextDouble();
+                    fin = initial*1.60935;
+                    System.out.println("Kilometers is " + fin);
+                    break;
+                case 2:
+                    System.out.println("Enter Kilometers:  ");
+                    initial = sc.nextDouble();
+                    fin = initial / 1.60935;
+                    System.out.println("Miles is " + fin);
+                    break;
+                case 3:
+                    System.out.println("Bye bye");
+                    cont = false;
+                    break;
+                default:
+                    System.out.println("I don't know that");
+                    break;
+            }
+        }
     }
 }
--- src/main/java/TempConverter.java
+++ src/main/java/TempConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TempConverter {
     public static void main(String[] args) {
         // TODO: Implement temperature converter
@@ -7,5 +9,36 @@ public class TempConverter {
         // - Use formulas: C = (F - 32) * 5/9 and F = C * 9/5 + 32
         // - Display conversion results
         // - Handle invalid menu choices
+        Scanner sc = new Scanner(System.in);
+        boolean cont = true;
+        System.out.println("Welcome to temperature converter");
+        int user = 0;
+        double initial = 0;
+        double fin = 0;
+        while (cont){
+            System.out.println("Convert to 1. Celsius 2. Farenheit or 3. Leave:  ");
+            user = sc.nextInt();
+            switch (user){
+                case 1:
+                    System.out.println("Enter Farenheit:  ");
+                    initial = sc.nextDouble();
+                    fin = (initial-32) * 5 / 9;
+                    System.out.println("Celsius is " + fin);
+                    break;
+                case 2:
+                    System.out.println("Enter Celsius:  ");
+                    initial = sc.nextDouble();
+                    fin = initial * (9.0 /5.0) + 32;
+                    System.out.println("Farenheit is " + fin);
+                    break;
+                case 3:
+                    System.out.println("Bye bye");
+                    cont = false;
+                    break;
+                default:
+                    System.out.println("I don't know that");
+                    break;
+            }
+        }
     }
 }
